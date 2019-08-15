210da22e087b195ed1c31ec987164ced17285aec
Everton <everton.mfernandes@gmail.com>
2019-08-15 14:40:28 -0300

    Especialização das variáveis dos casos de teste

--- core/codeGenerators/TestCaseCodeGenerator.py
+++ core/codeGenerators/TestCaseCodeGenerator.py
@@ -23,31 +23,49 @@ class TestCaseCodeGenerator(codeGenerator):
         queryParams = []
         order = []
         body = []
+        descriptionPath = ''
+        defaultVar = ''
+        compareValue = ''
 
         with open(storagePathFile) as datafile:
             columnInfo = csv.reader(datafile, delimiter=';')
             for column in columnInfo:
-                compare += ''.rjust(8)+'oResult:assertTrue(oJson["'+ column[1] +'"] == '+ column[7] +', "Valor comparado na coluna '+ column[0] +' de alias '+ column[1] +', nao sao iguais. Retorno:" + cRet)  \n'
-                compareAll += ''.rjust(8)+'oResult:assertTrue(oJson["items"][1]["'+ column[1] +'"] == '+ column[7] +', "Valor comparado na coluna '+ column[0] +' de alias '+ column[1] +', nao sao iguais. Retorno:" + cRet)  \n'
+                compareValue = column[7]
+                #ajusto a declaração default da variável
+                if column[7][0].upper() == "C":
+                    defaultVar = '""'
+                elif column[7][0].upper() == "N":
+                    defaultVar = "0"
+                elif column[7][0].upper() == "D":
+                    defaultVar = 'StoD("")'
+                    compareValue = 'DtoS('+column[7]+')'
+                else:
+                    defaultVar = "Nil"
+
+                compare += ''.rjust(8)+'oResult:assertTrue(oJson["'+ column[1] +'"] == '+ compareValue +', "Valor comparado na coluna '+ column[0] +' de alias '+ column[1] +', nao sao iguais. Retorno:" + cRet)  \n'
+                compareAll += ''.rjust(8)+'oResult:assertTrue(oJson["items"][1]["'+ column[1] +'"] == '+ compareValue +', "Valor comparado na coluna '+ column[0] +' de alias '+ column[1] +', nao sao iguais. Retorno:" + cRet)  \n'
                 order.append(column[1])
                 if column[2] == 'float':
                     body.append(''.rjust(24)+'\' "'+ column[1] +'": \'+AllTrim(Str('+ column[7] +'))+\'')
                 else:
-                    body.append(''.rjust(24)+'\' "'+ column[1] +'": "\'+'+ column[7] +'+\'"')
-                
+                    body.append(''.rjust(24)+'\' "'+ column[1] +'": "\'+'+ compareValue +'+\'"')
+
                 if column[5] == "1":
-                    keyCollumn = column[1]
+                    keyCollumn = column[7]
                 if column[4] == "1":
-                    keyVariables += ''.rjust(4)+'Local '+ column[7] +' := Nil /*<- change this value*/\n'
+                    keyVariables += ''.rjust(4)+'Local '+ column[7] +' := '+defaultVar+' /*<- change this value - Column ' + column[0] + '*/\n'
                     if column[5] == "0":
-                        queryParams.append(''.rjust(20)+'"&'+ column[1] +'="+'+ column[1] +'+;')
+                        queryParams.append(''.rjust(20)+'"&'+ column[1] +'="+escape('+ column[7] +')+;')
                 else:
-                    noKeyVariables += ''.rjust(4)+'Local '+ column[7] +' := Nil /*<- change this value*/\n'
+                    noKeyVariables += ''.rjust(4)+'Local '+ column[7] +' := '+defaultVar+' /*<- change this value - Column ' + column[0] + '*/\n'
             
+            descriptionPath = self.name.title().replace(" ","")
+            descriptionPath = descriptionPath[0].lower() + descriptionPath[1:]
             queryParams = '\n'.join(queryParams)
             order = ','.join(order)
             body = ',\' +;\n'.join(body)
             variables = {
+                    'descriptionPath': descriptionPath, 
                     'className': self.shortName, 
                     'entity' : self.entity,
                     'alias' : self.alias,
